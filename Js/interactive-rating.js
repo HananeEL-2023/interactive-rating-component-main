bf3437e0c79e72e9b1e24f2dfe460359c83c9054
Hanane <hanane.elkaaba1@gmail.com>
2023-09-24 16:41:51 +0100

    interactive-rating-component

---
+++ Js/interactive-rating.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("form");
+const buttons = document.querySelectorAll(".btn");
+const submitBtn = document.getElementById("submit");
+const result = document.getElementById("result");
+const popup = document.getElementById("popup");
+let ratingClicked = "";
+
+function handleClick() {
+  ratingClicked = this.dataset.rating;
+}
+buttons.forEach((button) => button.addEventListener("click", handleClick));
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (ratingClicked == "") {
+    alert("Please click a button first");
+  } else {
+    result.innerHTML = `${ratingClicked}`;
+    popup.style.visibility = "visible";
+  }
+});
